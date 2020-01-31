272508a22cb1834533ab8f3c24a16405704d0da0
Yicheng Shen <jsszsyc@hotmail.com>
2020-01-31 12:13:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pandas as pd
 
 
+def read_file(io):
+    f = open(io, "r")
+    contents = f.read()
+    f.close()
+    return contents
+
+def process_data(all_groups):
+    return
+
+
 def main():
+    """run this"""
+    file_io = 'test_data.txt'
+    contents = read_file(file_io)
+    process_data(contents)
     return
 
 
 
-
-file_io = 'test_data.txt'
+main()
